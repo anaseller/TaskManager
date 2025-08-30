e2f1d527fe51d626b2b3fc83199ac5ec02734651
anaseller <seller.anst@gmail.com>
2025-08-30 14:53:38 +0200

    create api

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from . import views
+from .views import (
+    TaskCreateView,
+    TaskListAPIView,
+    TaskDetailAPIView,
+    TaskStatisticsAPIView
+)
 
 urlpatterns = [
-    # ваши URL-шаблоны будут здесь
+    path('create/', TaskCreateView.as_view(), name='task-create'),
+    path('', TaskListAPIView.as_view(), name='task-list'),
+    path('<int:id>/', TaskDetailAPIView.as_view(), name='task-detail'),
+    path('statistics/', TaskStatisticsAPIView.as_view(), name='task-statistics'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.db.models import Count
+from django.utils import timezone
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskCreateView(generics.CreateAPIView):
+    """
+    Эндпоинт для создания новой задачи
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskListAPIView(generics.ListAPIView):
+    """
+    Эндпоинт для получения списка всех задач
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetailAPIView(generics.RetrieveAPIView):
+    """
+    Эндпоинт для получения конкретной задачи по ее id
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
+
+
+class TaskStatisticsAPIView(APIView):
+    """
+    Эндпоинт для получения статистики задач
+    """
+
+    def get(self, request):
+        total_tasks = Task.objects.count()
+        tasks_by_status = Task.objects.values('status').annotate(count=Count('status'))
+
+        overdue_tasks = Task.objects.filter(
+            deadline__lt=timezone.now(),
+            status__in=['New', 'In progress', 'Pending', 'Blocked']
+        ).count()
+
+        statistics = {
+            'total_tasks': total_tasks,
+            'tasks_by_status': list(tasks_by_status),
+            'overdue_tasks': overdue_tasks
+        }
+        return Response(statistics)
