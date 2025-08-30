3b5e4b343932efb5809b954210c8e08aba980ca7
anaseller <seller.anst@gmail.com>
2025-08-30 23:55:34 +0200

    update

--- src/tasks/pagination.py
+++ src/tasks/pagination.py
@@ -1,7 +1,10 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import CursorPagination, PageNumberPagination
 
 class StandardResultsSetPagination(PageNumberPagination):
-
     page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 50
+
+class CustomCursorPagination(CursorPagination):
+    page_size = 6
+    ordering = '-created_at'
